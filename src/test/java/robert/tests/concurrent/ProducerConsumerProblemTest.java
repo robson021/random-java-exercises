73600dcda92e70285fbc28209d7c35af786dea85
robert <nowak021@gmail.com>
2017-06-04 21:04:26 +0200

    add producer - consumer problem example with blocking queue

---
+++ src/test/java/robert/tests/concurrent/ProducerConsumerProblemTest.java
@@ -0,0 +1,14 @@
+package robert.tests.concurrent;
+
+import org.junit.Test;
+
+import robert.concurrent.ProducerConsumerProblem;
+
+public class ProducerConsumerProblemTest {
+
+	@Test
+	public void test() throws Exception {
+		new ProducerConsumerProblem().startExample();
+	}
+
+}
