6241c54c20518bbb669666e3c410ad6f37954da4
robert <nowak021@gmail.com>
2017-06-04 21:58:32 +0200

    add producer - consumer problem example with manual synchronization

--- src/test/java/robert/tests/concurrent/ProducerConsumerProblemTest.java
+++ src/test/java/robert/tests/concurrent/ProducerConsumerProblemTest.java
@@ -2,6 +2,7 @@ package robert.tests.concurrent;
 
 import org.junit.Test;
 
+import robert.concurrent.ProducerConsumer2;
 import robert.concurrent.ProducerConsumerProblem;
 
 public class ProducerConsumerProblemTest {
@@ -9,6 +10,12 @@ public class ProducerConsumerProblemTest {
 	@Test
 	public void test() throws Exception {
 		new ProducerConsumerProblem().startExample();
+	}
+
+	@Test
+	public void test2() throws Exception {
+		ProducerConsumer2 producerConsumer = new ProducerConsumer2();
+		producerConsumer.startExample();
 	}
 
 }
